ec1525b41ac8059cb41159cd9332a5fbbe5ed62b
Jinpeng Zhang <zhangjp19@gmail.com>
2025-06-10 22:32:22 +0800

    Update paramDictProperties 1. Add pEnm, pEnmOpt, pEnmList, pEnmListOpt 2. Add typing.List to adapt earlier python

--- paramDictProperties.py
+++ paramDictProperties.py
@@ -1,7 +1,7 @@
 from .localization import l10n
 _ = l10n.gettext
 
-from typing import get_type_hints, Callable
+from typing import get_type_hints, Callable, List
 
 from enum import Enum
 
@@ -12,7 +12,8 @@ from .colorOutput import outGreen, outRed, outYellow
 __all__ = ['pSimple', 'pSimpleOpt', 'pStrict', 'pStrictOpt',
            'UNSET', 'SET',
            'pList', 'pListOpt',
-           'pPd', 'pPdOpt', 'pPdList', 'pPdListOpt']
+           'pPd', 'pPdOpt', 'pPdList', 'pPdListOpt',
+           'pEnm', 'pEnmOpt', 'pEnmList', 'pEnmListOpt']
 
 class ValueStatus(Enum):
     UNSET = 0
@@ -118,10 +119,10 @@ class pList(pdprop):
     def __set__(self, instance:baseParamDict, value):
         aType:type = get_type_hints(self.fget)['return'].__args__[0]
         if not isinstance(value, list):
-            raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(list[aType])))
+            raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
         for item in value:
             if not isinstance(item, aType):
-                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(list[aType])))
+                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
         instance._cont[self.fget.__name__] = value
         setattr(instance, self._status_name, 1)
 
@@ -135,10 +136,10 @@ class pListOpt(pdprop):
     def __set__(self, instance:baseParamDict, value):
         aType:type = get_type_hints(self.fget)['return'].__args__[0]
         if not isinstance(value, list):
-            raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(list[aType])))
+            raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
         for item in value:
             if not isinstance(item, aType):
-                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(list[aType])))
+                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
         instance._cont[self.fget.__name__] = value
         setattr(instance, self._status_name, 1)
     
@@ -198,7 +199,7 @@ class pPdList(pdprop):
         aType:type = get_type_hints(self.fget)['return'].__args__[0]
         for item in value:
             if not isinstance(item, aType):
-                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(list[aType])))
+                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
         instance._cont[self.fget.__name__] = [item.content for item in value]
         setattr(instance, self._status_name, 1)
 
@@ -214,8 +215,84 @@ class pPdListOpt(pdprop):
         aType:type = get_type_hints(self.fget)['return'].__args__[0]
         for item in value:
             if not isinstance(item, aType):
-                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(list[aType])))
+                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
         instance._cont[self.fget.__name__] = [item.content for item in value]
+        setattr(instance, self._status_name, 1)
+    
+    def __delete__(self, instance:baseParamDict):
+        status = getattr(instance, self._status_name, 0)
+        if status == 0:
+            return
+        instance._cont.pop(self.fget.__name__, None)
+        setattr(instance, self._status_name, 0)
+
+class pEnm(pdprop):
+    def __get__(self, instance:baseParamDict, owner):
+        status = getattr(instance, self._status_name, 0)
+        if status == 0:
+            raise AttributeError(_('Property %s not set.') % outYellow('\''+self.fget.__name__+'\''))
+        aType:type = get_type_hints(self.fget)['return']
+        return aType(instance._cont[self.fget.__name__])
+    
+    def __set__(self, instance:baseParamDict, value:Enum):
+        aType:type = get_type_hints(self.fget)['return']
+        if not isinstance(value, aType):
+            raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(aType)))
+        instance._cont[self.fget.__name__] = value.value
+        setattr(instance, self._status_name, 1)
+
+class pEnmOpt(pdprop):
+    def __get__(self, instance:baseParamDict, owner):
+        status = getattr(instance, self._status_name, 0)
+        if status == 0:
+            return UNSET
+        aType:type = get_type_hints(self.fget)['return']
+        return aType(instance._cont[self.fget.__name__])
+    
+    def __set__(self, instance:baseParamDict, value:Enum):
+        aType:type = get_type_hints(self.fget)['return']
+        if not isinstance(value, aType):
+            raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(aType)))
+        instance._cont[self.fget.__name__] = value.value
+        setattr(instance, self._status_name, 1)
+    
+    def __delete__(self, instance:baseParamDict):
+        status = getattr(instance, self._status_name, 0)
+        if status == 0:
+            return
+        instance._cont.pop(self.fget.__name__, None)
+        setattr(instance, self._status_name, 0)
+
+class pEnmList(pdprop):
+    def __get__(self, instance:baseParamDict, owner):
+        status = getattr(instance, self._status_name, 0)
+        if status == 0:
+            raise AttributeError(_('Property %s not set.') % outYellow('\''+self.fget.__name__+'\''))
+        aType:type = get_type_hints(self.fget)['return'].__args__[0]
+        return [aType(item) for item in instance._cont[self.fget.__name__]]
+    
+    def __set__(self, instance:baseParamDict, value:'list[Enum]'):
+        aType:type = get_type_hints(self.fget)['return'].__args__[0]
+        for item in value:
+            if not isinstance(item, aType):
+                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
+        instance._cont[self.fget.__name__] = [item.value for item in value]
+        setattr(instance, self._status_name, 1)
+
+class pEnmListOpt(pdprop):
+    def __get__(self, instance:baseParamDict, owner):
+        status = getattr(instance, self._status_name, 0)
+        if status == 0:
+            return UNSET
+        aType:type = get_type_hints(self.fget)['return'].__args__[0]
+        return [aType(item) for item in instance._cont[self.fget.__name__]]
+    
+    def __set__(self, instance:baseParamDict, value:'list[Enum]'):
+        aType:type = get_type_hints(self.fget)['return'].__args__[0]
+        for item in value:
+            if not isinstance(item, aType):
+                raise TypeError(_('Property %s must be of type %s.') % (outYellow('\''+self.fget.__name__+'\''), outGreen(List[aType])))
+        instance._cont[self.fget.__name__] = [item.value for item in value]
         setattr(instance, self._status_name, 1)
     
     def __delete__(self, instance:baseParamDict):
