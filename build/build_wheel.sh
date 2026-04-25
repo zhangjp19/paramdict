734d73a6bcfdca8ada985941d21e28c421bc3ce9
zhangjp19 <zhangjp19@gmail.com>
2026-04-25 17:30:50 +0800

    Add project build files.

---
+++ build/build_wheel.sh
@@ -0,0 +1,37 @@
+directory_list=(
+    "locale"
+)
+target_dir="paramdict"
+target_files=(
+    "LICENSE"
+    "README.md"
+)
+
+pyfilenum=$(ls "$target_dir" | grep "\.py$" | wc -l)
+if [ "$pyfilenum" -gt 0 ]; then
+    rm "$target_dir"/*.py
+fi
+for dir in "${directory_list[@]}"; do
+    if [ -d "$target_dir/$dir" ]; then
+        rm -r "$target_dir/$dir"
+    fi
+done
+for file in "${target_files[@]}"; do
+    if [ -f "$file" ]; then
+        rm "$file"
+    fi
+done
+
+if [ -d "build" ]; then
+    rm -r build
+fi
+
+cp ../*.py "$target_dir"/
+for dir in "${directory_list[@]}"; do
+    cp -r ../"$dir" "$target_dir"/
+done
+for file in "${target_files[@]}"; do
+    cp ../$file ./
+done
+
+python -m build --wheel
